4ba584127f0fb847ad3ab2fed3a899b5e04d3f7a
kockono <kockonoop@gmail.com>
2022-08-03 04:25:06 -0600

    new comments

--- src/middlewares/validate-fields.js
+++ src/middlewares/validate-fields.js
@@ -7,15 +7,17 @@ const { validationResult } = require('express-validator');
  * @validationResult
  * @var { errors } - Contiene los mensajes de errores escritos en  "src/routes/users.routes.js
  * 
- * (req, res, next)
+ * @req  - Los valores que recibe la ruta, la peticion
+ * @res  - La respuesta a tu peticiones
  * @next - Si es valido continuar la petición, si no terminala
  * @status_400 - Bad Request
  */
 
 
 const validateForms = ( req, res = response, next ) => {
-
-  const errors = validationResult( req );    // Arreglo de errores
+ 
+  // Arreglo de errores
+  const errors = validationResult( req );   
 
   // Reviso si existen errores
   if(!errors.isEmpty()) { 

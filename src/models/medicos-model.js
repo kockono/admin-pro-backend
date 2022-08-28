04a01d21e9afe2ee2a424d1db350e980dbefa6b2
kockono <kockonoop@gmail.com>
2022-08-28 05:35:15 -0600

    medicos model

---
+++ src/models/medicos-model.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+/**
+ * @var trim    - Elimina los espacios      
+ * @var type    - Tipado del dato   
+ * @var default - Dato asignado por default
+ * @var Schema.Types.ObjectId - Esto le dice a mongoose que hara una refererencia a una id de un schema
+ * @var collection - Nos permite renombrar en la base de datos el nombre de la tabla ( Siempre agrega una 'S')
+ */
+
+let MedicosSchema = new Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        img: {
+          type: String
+        }, 
+        usuario: { 
+          type: Schema.Types.ObjectId,
+          ref:'Users'
+        },
+        hospital: { 
+            type: Schema.Types.ObjectId,
+            ref:'hospital'
+          },
+        status:{
+          type: Number,
+          default: 1
+        }
+}, { collection: 'Medicos', timestamps: true }); // Timestamps tiempos de creacion del dato, Collection: nombre de la db
+
+
+// Elimina el __v a la hora de guardar en mongodb
+MedicosSchema.method('toJSON', function(){
+  const {__v, ...object } = this.toObject();
+  return object;
+});
+
+module.exports = model('Medicos', MedicosSchema);

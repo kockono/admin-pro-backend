5e8c276b5c0dbb3b1ef2a5e8dafc650eef935005
kockono <kockonoop@gmail.com>
2022-09-02 04:34:45 -0600

    Busqueda routes

---
+++ src/routes/busquedas.routes.js
@@ -0,0 +1,45 @@
+const { Router } = require('express');
+
+// Middlewares
+const { validateJwt } = require('../middlewares/validate-jwt');
+
+// Controllers
+const { getMedicos, createMedico, updateMedico, deleteMedico } = require('../controllers/medicos-controller');
+
+// Validadors de body, formularios, post
+const { check } = require('express-validator'); // Version  6.14.1
+const { validateForms } = require('../middlewares/validate-fields');  
+
+const router = Router();
+
+/**
+ * ----------------------------- Apis -----------------------------------
+ * @api_createMedico  POST http://localhost:3501/api/medicos/create  
+ * @api_getMedicos    GET  http://localhost:3501/api/medicos/        
+ * 
+ */
+ 
+
+ router.get('/:busqueda',  getMedicos );
+
+ router.post('/create', 
+   [
+    validateJwt,
+    check('name', 'El name es obligatorio').not().isEmpty(),
+    check('hospital','La id mandada no es válida').isMongoId(), // Valida el Id de MongoDb
+    validateForms 
+   ],
+   createMedico
+ );
+ 
+ router.put('/:id',
+     [
+
+     ],
+     updateMedico
+ );
+ 
+ router.put('/delete/:id', deleteMedico);
+
+
+module.exports = router;

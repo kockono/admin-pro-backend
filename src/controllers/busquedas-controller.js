85eb170ad13a602d10a2610bb0f2a1dd2d765b03
personal <kockonoop@gmail.com>
2023-01-11 02:07:00 -0600

    Controlador de buesqueda

--- src/controllers/busquedas-controller.js
+++ src/controllers/busquedas-controller.js
@@ -1,28 +1,78 @@
 const { response } = require('express');
 
-/** ----------------------------------- Controllers -----------------------------------
-* @function {@link getTodo()}   - Traer todos los usuarios 
+// Modelos
+const Usuario = require('../models/user-model');
+const Hospital = require('../models/hospital-model');
+const Medico = require('../models/medicos-model');
+
+/** ------------------------------------ Controllers --------------------------------------
+* @function {@link getTodo()}       - Traer todos los Usuarios por coincidencia de busqueda
+* @function {@link getDocumentosColeccion()}    - Traer todos los Medicos por coincidencia de busqueda
 * 
 */
 
-// Todo: Video 130 Minuto 4:00, Continuar con el buscador
 const getTodo = async (req, res = response ) => {
 
-    const objetoBuscado = req.body.busqueda;
-    
-    const medico = await MedicoModel.find({ status: 1}, 'name email role google')
-                                      .populate('hospital', 'name')
-                                      .populate('usuario', 'name');
+    const busqueda = req.params.busqueda;
+    const regex    = new RegExp(busqueda, 'i'); // Like permite ser mas flexible en la busqueda
+
+    // Este proceso toma 3 veces mas tiempo de lo normal
+    // const usuarios   = await Usuario.find({ nombre: regex });
+    // const medicos    = await Medico.find({ nombre: regex });
+    // const hospitales = await Hospital.find({ nombre: regex });
+
+    // Así lo hace todas al mismo tiempo, de manera simultanea
+    const [usuarios, medicos, hospitales] = await Promise.all([
+        Usuario.find({ nombre: regex }),
+        Medico.find({ nombre: regex }),
+        Hospital.find({ nombre: regex })
+    ]);
   
-      res.json({
+    res.json({
         ok:true,
-        msg: 'Get medico',
-        medicos: [medico]
+        usuarios,
+        medicos,
+        hospitales
       });
   
-  
-  }
+}
 
-  module.exports = {
-    getTodo
-  }
+const getDocumentosColeccion = async (req, res = response ) => {
+    console.log( req.body )
+    const tabla = req.params.tabla;
+    const busqueda = req.params.busqueda;
+    const regex    = new RegExp(busqueda, 'i'); // Like permite ser mas flexible en la busqueda
+
+    let data = [];
+
+    switch ( tabla ) {
+        case 'medicos':
+            data = await Medico.find({ nombre: regex }).populate('usuario', 'nombre img')
+                                                       .populate('hospital', 'nombre img');
+        break;
+        case 'hospitales':
+            data = await Hospital.find({ nombre: regex }).populate('usuario', 'nombre img');
+        break;
+        case 'usuarios':
+            data = await Usuario.find({ nombre: regex });
+        break;
+        default:
+            return  res.status(400).json({
+                        ok: false,
+                        msg: 'La tabla tiene que ser usuarios/medicos/hospitales'
+                    });
+    }
+
+    res.json({
+        ok:true,
+        resultados: data
+    })
+}
+
+
+
+module.exports = {
+    getTodo,
+    getDocumentosColeccion
+
+}
--- src/routes/busquedas.routes.js
+++ src/routes/busquedas.routes.js
@@ -4,7 +4,7 @@ const { Router } = require('express');
 const { validateJwt } = require('../middlewares/validate-jwt');
 
 // Controllers
-const { getTodo } = require('../controllers/busquedas-controller');
+const { getTodo, getDocumentosColeccion } = require('../controllers/busquedas-controller');
 
 // Validadors de body, formularios, post, put, delete, update
 const { check } = require('express-validator'); // Version  6.14.1
@@ -14,30 +14,15 @@ const router = Router();
 
 /**
  * ----------------------------- Apis -----------------------------------
- * @api_busqueda GET http://localhost:3501/api/todo/:busqueda  
+ * @api_busqueda GET http://localhost:3501/api/todo/:busqueda 
+ * @api GET: getDocumentosColeccion : Me trae elementos de hospitales, medicos o usuarios
  * 
  */
  
- router.get('/:busqueda',  validarJWT, getTodo );
+ router.get('/:busqueda',  validateJwt, getTodo );
 
- router.post('/create', 
-   [
-    validateJwt,
-    check('name', 'El name es obligatorio').not().isEmpty(),
-    check('hospital','La id mandada no es válida').isMongoId(), // Valida el Id de MongoDb
-    validateForms 
-   ],
-   createMedico
- );
- 
- router.put('/:id',
-     [
+ router.get('/coleccion/:tabla/:busqueda',  validateJwt, getDocumentosColeccion );
 
-     ],
-     updateMedico
- );
- 
- router.put('/delete/:id', deleteMedico);
 
 
 module.exports = router;

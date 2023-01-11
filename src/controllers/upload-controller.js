5ddd8ef61ee1fd016320d24eca4b8ecf7bd7a908
personal <kockonoop@gmail.com>
2023-01-11 05:44:21 -0600

    feat: Upload controller and route
    Controller for upload images in format jpg, png, gif, jpeg

--- src/controllers/upload-controller.js
+++ src/controllers/upload-controller.js
@@ -1,25 +1,71 @@
 const { response } = require('express');
 
+const { v4: uuidv4 } = require('uuid');
+
 const fileUpload = ( req, res = response ) => {
 
-const tipo = req.params.tipo;
-const id   = req.params.id;
+    const tipo = req.params.tipo;
+    const id   = req.params.id;
 
-const tipoValidos = ['hospitales', 'medicos', 'usuarios'];
+    const tipoValidos = ['hospitales', 'medicos', 'usuarios'];
 
-if( tipoValidos.includes(tipo)) {
-    return res.status(400).json({
-        ok:false,
-        msg: 'No es un médico, usuario u hospital'
-    })
+    if( !tipoValidos.includes(tipo)) {
+        return res.status(400).json({
+            ok:false,
+            msg: 'No es un médico, usuario u hospital'
+        })
+    }
+
+    // Valida que exista un archivo
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({
+            ok:false,
+            msg: 'No hay ningún archivo'
+        });
+    }
+
+    // Procesar la imagen...
+    const imagen = req.files.imagen;
+
+    const nombreCortado = imagen.name.split('.'); // ejemplo.1.2.jpg
+    const extensionArchivo = nombreCortado[ nombreCortado.length - 1]; // Ultima posición
+
+    // Validar extension
+    if( validarExtension(extensionArchivo) ) {
+        return res.status(400).json({
+            ok:false,
+            msg: 'No es una extensión permitida'
+        });
+    }
+    console.log( extensionArchivo )
+    // Generar el nombre del archivo
+    const nombreArchivo = `${uuidv4()}.${extensionArchivo}`;
+
+    // Path para guardar la imagen
+    const path = `./src/uploads/${ tipo }/${ nombreArchivo }`;
+    // Usar mv para mover la imagen donde quieras
+    imagen.mv(path, (err) => {
+        if (err){
+            console.log( err )
+            return res.status(500).json({
+                msg: 'Error al mover la imagen'
+            });
+        }
+        res.json({
+            ok: true,
+            nombreArchivo    
+        })
+    });
+
+}
+
+function validarExtension( extensionArchivo ) {
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+    return !extensionesValidas.includes(extensionArchivo);
 }
 
 
-    res.json({
-        ok: true,
-        msg: 'fileUploaded'
-    })
-}
+
 
 module.exports = {
     fileUpload
--- src/routes/uploads.routes.js
+++ src/routes/uploads.routes.js
@@ -6,8 +6,13 @@ const { fileUpload } = require('../controllers/upload-controller');
 // Validadors de body, formularios, post
 const { validateJwt } = require('../middlewares/validate-jwt');
 
+// Middlewares
+const expressFileUpload = require('express-fileupload');
+
 const router = Router();
 
+// default options
+router.use(expressFileUpload());
 
 router.put('/:tipo/:id', validateJwt, fileUpload);
 
